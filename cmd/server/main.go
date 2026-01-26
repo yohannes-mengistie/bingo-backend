f407fdc4da72f4bf19833f1d279ef164af05dd5c
Yonatannnn <yonatanalebachew7@gmail.com>
2026-01-26 13:53:06 +0300

    cors

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -127,7 +127,7 @@ func setupRouter(userHandler *handler.UserHandler, walletHandler *handler.Wallet
 
 	// CORS middleware
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000", "https://winner.up.railway.app"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000", "https://winner.up.railway.app", "https://bingo-frontend-production-7ee9.up.railway.app/"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
