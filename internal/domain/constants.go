773ed6b6b01248fb95c21f5636ea0c3151a5def4
Yonatannnn <yonatanalebachew7@gmail.com>
2026-01-30 18:26:23 +0300

    DrawInterval increased

--- internal/domain/constants.go
+++ internal/domain/constants.go
@@ -147,7 +147,7 @@ const MaxAvailableGamesLimit = 50
 // Drawing Constants
 
 // DrawInterval is the interval between drawing numbers
-const DrawInterval = 1 * time.Second
+const DrawInterval = 250 * time.Millisecond
 
 // WebSocket Handler Constants
 
